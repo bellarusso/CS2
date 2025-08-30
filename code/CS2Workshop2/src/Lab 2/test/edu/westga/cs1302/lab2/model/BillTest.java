682920492f3750412e2aac6404f6aeedd2b0d97a
bellarusso <Irusso1@my.westga.edu>
2025-08-30 18:35:35 -0400

    created test file for Bill class

---
+++ code/CS2Workshop2/src/Lab 2/test/edu/westga/cs1302/lab2/model/BillTest.java
@@ -0,0 +1,14 @@
+package edu.westga.cs1302.lab2.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BillTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
