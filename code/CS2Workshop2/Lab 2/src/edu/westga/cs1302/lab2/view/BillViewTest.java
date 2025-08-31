258187ddb4f612f7f374dc84a4c40cf29c9cf1bb
bellarusso <Irusso1@my.westga.edu>
2025-08-30 20:10:03 -0400

    create test for BillView

---
+++ code/CS2Workshop2/Lab 2/src/edu/westga/cs1302/lab2/view/BillViewTest.java
@@ -0,0 +1,14 @@
+package edu.westga.cs1302.lab2.view;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BillViewTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
