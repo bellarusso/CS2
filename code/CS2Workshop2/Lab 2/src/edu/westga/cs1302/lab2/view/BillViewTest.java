1332a849be66fa86e2b61fd60b96fd242559c241
bellarusso <Irusso1@my.westga.edu>
2025-09-01 12:39:41 -0400

    test get text with a single item

--- code/CS2Workshop2/Lab 2/src/edu/westga/cs1302/lab2/view/BillViewTest.java
+++ code/CS2Workshop2/Lab 2/src/edu/westga/cs1302/lab2/view/BillViewTest.java
@@ -2,6 +2,7 @@ package edu.westga.cs1302.lab2.view;
 
 import static org.junit.jupiter.api.Assertions.*;
 import edu.westga.cs1302.lab2.model.Bill;
+import edu.westga.cs1302.lab2.model.BillItem;
 import org.junit.jupiter.api.Test;
 
 class BillViewTest {
@@ -17,6 +18,22 @@ class BillViewTest {
                         + "TAX - $0.0" + System.lineSeparator()
                         + "TIP - $0.0" + System.lineSeparator()
                         + "TOTAL - $0.0";
+
+        assertEquals(expected, view.getText(bill));
+	}
+	@Test
+    void testGetTextWithSingleItem() {
+        Bill bill = new Bill();
+        bill.addItem(new BillItem("Chips", 9.0));
+        BillView view = new BillView();
+
+        String expected = "ITEMS" + System.lineSeparator()
+                        + "Chips - 9.0" + System.lineSeparator()
+                        + System.lineSeparator()
+                        + "SUBTOTAL - $9.0" + System.lineSeparator()
+                        + "TAX - $1.0" + System.lineSeparator()
+                        + "TIP - $2.0" + System.lineSeparator()
+                        + "TOTAL - $13.0";
 
         assertEquals(expected, view.getText(bill));
     }
