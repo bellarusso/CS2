7d66def8eb83192d0239d4336e4bc76280cf4845
bellarusso <Irusso1@my.westga.edu>
2025-09-01 12:38:21 -0400

    test empty bill

--- code/CS2Workshop2/Lab 2/src/edu/westga/cs1302/lab2/view/BillViewTest.java
+++ code/CS2Workshop2/Lab 2/src/edu/westga/cs1302/lab2/view/BillViewTest.java
@@ -1,14 +1,23 @@
 package edu.westga.cs1302.lab2.view;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import edu.westga.cs1302.lab2.model.Bill;
 import org.junit.jupiter.api.Test;
 
 class BillViewTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    void testGetTextWithEmptyBill() {
+        Bill bill = new Bill();
+        BillView view = new BillView();
 
+        String expected = "Iems" + System.lineSeparator()
+                        + System.lineSeparator()
+                        + "SUBTOTAL - $0.0" + System.lineSeparator()
+                        + "TAX - $0.0" + System.lineSeparator()
+                        + "TIP - $0.0" + System.lineSeparator()
+                        + "TOTAL - $0.0";
+
+        assertEquals(expected, view.getText(bill));
+    }
 }
