05b65dfaff24ad6d2c830eada7e2f57d47495ef9
bellarusso <Irusso1@my.westga.edu>
2025-09-01 12:34:02 -0400

    test for adding multiple items

--- code/CS2Workshop2/Lab 2/src/edu/westga/cs1302/lab2/model/BillTest.java
+++ code/CS2Workshop2/Lab 2/src/edu/westga/cs1302/lab2/model/BillTest.java
@@ -1,7 +1,7 @@
 package edu.westga.cs1302.lab2.model;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import java.util.ArrayList;
 import org.junit.jupiter.api.Test;
 
 class BillTest {
@@ -19,5 +19,21 @@ class BillTest {
 	        bill.addItem(item1);
 	        assertEquals(1, bill.getItems().size(), "Bill should have atleast 1 item");
 	        assertEquals(item1, bill.getItems().get(0), "Added item should be named item1");
+	    }
+	 @Test
+	 void testAddMultipleItems() {
+	        bill.addItem(item1);
+	        bill.addItem(item2);
+	        ArrayList<BillItem> items = bill.getItems();
+	        assertEquals(2, items.size(), "Bill should have 2 items after adding two");
+	        assertTrue(items.contains(item1), "Bill should contain item1");
+	        assertTrue(items.contains(item2), "Bill should contain item2");
+	    }
+	 @Test
+	    void testAddItemNullThrowsException() {
+	        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+	            bill.addItem(null);
+	        });
+	        assertEquals("item must not be null.", exception.getMessage());
 	    }
 }
